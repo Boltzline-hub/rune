419772430bcaaf67bbecd57abe3d2d56664328ed
Boltzline <auguste542lavi@gmail.com>
2023-12-13 22:48:31 +0100

    hoo

--- boltline/src/_auth/forms/Signupform.tsx
+++ boltline/src/_auth/forms/Signupform.tsx
@@ -1,11 +1,65 @@
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
+import { useForm, SubmitHandler, UseFormReturn } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-const Signupform = () => {
+// Define your form schema
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+// Define your form component
+const Signupform: React.FC = () => {
+  // Define your form using useForm
+  const form: UseFormReturn<z.infer<typeof formSchema>> = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // Define your submit handler
+  const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (values) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  };
+
   return (
     <div>
-      <Button>Click me</Button>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Signupform
+export default Signupform;
