a03cfb9282e40927a7c5e10dea222ec4d75d6380
Boltzline <auguste542lavi@gmail.com>
2023-12-17 11:45:22 +0100

    updating logo and finalising auth

--- boltline/src/_auth/forms/Signupform.tsx
+++ boltline/src/_auth/forms/Signupform.tsx
@@ -1,23 +1,19 @@
+import React from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { useForm, SubmitHandler, UseFormReturn } from "react-hook-form";
-import { Form, FormControl, FormField,  FormItem, FormLabel, FormMessage,}from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
 import { Loader } from "lucide-react";
 import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
-
-// Define your form schema
-
-
-// Define your form component
 const Signupform: React.FC = () => {
-  const isloading = false;
+  const [isLoading, setIsLoading] = React.useState(false);
 
-  // Define your form using useForm
-  const form: UseFormReturn<z.infer<typeof SignupValidation >> = useForm({
+  const form: UseFormReturn<z.infer<typeof SignupValidation>> = useForm({
     resolver: zodResolver(SignupValidation),
     defaultValues: {
       name: "",
@@ -27,21 +23,33 @@ const Signupform: React.FC = () => {
     },
   });
 
-  // Define your submit handler
-  const onSubmit: SubmitHandler<z.infer<typeof SignupValidation>> = (values) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  const onSubmit: SubmitHandler<z.infer<typeof SignupValidation>> = async (values) => {
+    try {
+      setIsLoading(true);
+      const newUser = await createUserAccount(values);
+      console.log(newUser);
+      // Add any additional logic for successful user creation
+    } catch (error) {
+      console.error("User creation error:", error.message);
+      // Add error handling and display relevant messages to the user
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
-      <Form {...form}>
-        <div className=" sm:428 flex-center  flex-col">
-          <img className="w-40" src="/assets/images/logo.svg"/>
-          <h2 className="h-5 md:h-10 pt-5 sm:pt-1"> Create a new account</h2>
-          <p className="text-light-3 small-medium md:base-regular">To use Snapgram enter your account details</p>
+    <Form {...form}>
+      <div className="sm:428 flex-center flex-col">
+        <div className="flex text-xl font-bold gap-2 items-center">
+          <img className="w-10" src="/assets/images/boltline.png" alt="BoltLine Logo" />
+          <h1>BoltLine</h1>
+        </div>
+        <h2 className="h-5 md:h-10 pt-5 sm:pt-1">Create a new account</h2>
+        <p className="text-light-3 small-medium md:base-regular">
+          To use Boltline, please enter your account details
+        </p>
 
-        <form onSubmit={form.handleSubmit(onSubmit)} className=" flex flex-col gap-5 w-full mt-4">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
           <FormField
             control={form.control}
             name="name"
@@ -55,7 +63,7 @@ const Signupform: React.FC = () => {
               </FormItem>
             )}
           />
-              <FormField
+          <FormField
             control={form.control}
             name="username"
             render={({ field }) => (
@@ -64,12 +72,11 @@ const Signupform: React.FC = () => {
                 <FormControl>
                   <Input type="text" className="shad-input" {...field} />
                 </FormControl>
-
                 <FormMessage />
               </FormItem>
-              )}
-            />
-            <FormField
+            )}
+          />
+          <FormField
             control={form.control}
             name="email"
             render={({ field }) => (
@@ -78,12 +85,11 @@ const Signupform: React.FC = () => {
                 <FormControl>
                   <Input type="email" className="shad-input" {...field} />
                 </FormControl>
-
                 <FormMessage />
               </FormItem>
             )}
           />
-                    <FormField
+          <FormField
             control={form.control}
             name="password"
             render={({ field }) => (
@@ -96,21 +102,26 @@ const Signupform: React.FC = () => {
               </FormItem>
             )}
           />
-          <Button type="submit" className="shad-button_primary">
-              {isloading ? (
-              <div className="flex-center gap-2"> 
-                <Loader/> 
+          <Button type="submit" className="shad-button_primary" disabled={isLoading}>
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Loader />
               </div>
-              ): "Sign Up"}
+            ) : (
+              "Sign Up"
+            )}
           </Button>
 
           <p className="text-small-regular text-light-2 text-center mt-2">
-            Already have an account?  
-            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1"> Log in</Link>
+            Already have an account?
+            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">
+              {" "}
+              Log in
+            </Link>
           </p>
         </form>
       </div>
-      </Form>
+    </Form>
   );
 };
 
